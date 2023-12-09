7eaf27fbcedcfaea732becdd49f86eda8916508b
Nikolay Mosalov <73695807+Rom-dev-tech@users.noreply.github.com>
2023-12-09 15:46:57 +0200

    update

--- src/js/unsplash-api.js
+++ src/js/unsplash-api.js
@@ -1,17 +1,20 @@
-import axios from "axios";
+import axios from 'axios';
 
-const BASE_URL = 'https://api.unsplash.com';
 const endPoint = '/search/photos';
 
+const instance = axios.create({
+  baseURL: 'https://api.unsplash.com',
+  timeout: 1000,
+  headers: { ['x-api-key']: 'LxvKVGJqiSe6NcEVZOaLXC-f2JIIWZaq_o0WrF8mwJc' },
+});
 
 export function getPhotos() {
-    return axios.get(`${BASE_URL}${endPoint}`, {
-        params: {
-            page: 1,
-            query: "cat",
-            client_id: "LxvKVGJqiSe6NcEVZOaLXC-f2JIIWZaq_o0WrF8mwJc"
-        }
-    })
+  return instance.get(`${endPoint}`, {
+    params: {
+      page: 1,
+      query: 'cat',
+      client_id: 'LxvKVGJqiSe6NcEVZOaLXC-f2JIIWZaq_o0WrF8mwJc',
+    },
+  });
 }
 getPhotos().then(data => console.log(data));
-
